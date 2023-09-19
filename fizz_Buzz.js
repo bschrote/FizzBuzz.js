64f4b9349e06b5690d4e95ae6a8c96ab91bcac14
bschrote <140435658+bschrote@users.noreply.github.com>
2023-09-18 22:28:21 -0400

    Add files via upload

--- fizz_Buzz.js
+++ fizz_Buzz.js
@@ -1,17 +1,48 @@
 let F = 3
 let B = 5
 
-for (let N = 1; N<=100; N++)
-{
-    if ((N % F == 0) && (N % B == 0)) 
+for (let I = 1; I<=100; I++){
+    let N = I.toString();
+    
+    if((N % F == 0) && (N % B == 0)) {
+        if ((N.includes(F)== true) && (N.includes(B) == true)) {
+            console.log ("FizzFizzBuzzBuzz")
+        }
+        if (N.includes(F) == true) {
+            console.log("FizzFizzBuzz")
+        } 
+        if (N.includes(B) == true) {
+            console.log("FizzBuzzBuzz")    
+        }      
+    }
+    else if (((N % F == 0) && (N % B == 0) ) || ((N.includes(F) == true) && (N.includes(B) == true))) { 
        console.log("FizzBuzz")
-    else if (N % F == 0)
-    {
+    }
+    
+    else if ((N % F == 0) && ((N.includes(F) == true) || (N.includes(B) == true))) {
+        if (N.includes(F) == true) {
+            console.log("FizzFizz")
+        }
+        if (N.includes(B) == true) {
+            console.log("FizzBuzz")
+        }
+    }     
+    else if ((N.includes(F) == true) || (N % F == 0))
+ {
         console.log ("Fizz")
     }
-    else if (N % B == 0) {
+    else if ((N % B == 0) && ((N.includes(F) == true) || (N.includes(B) == true))) {
+        if (N.includes(B) == true) {
+        console.log ("BuzzBuzz")
+        }
+        if (N.includes(F) == true) {
+            console.log("FizzBuzz")
+        }
+        } 
+    else if ((N.includes(B) == true) || (N % B == 0)) {
         console.log ("Buzz")
     } 
+   
     else {
         console.log(N)
         }
